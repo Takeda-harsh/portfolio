a66494443ea7d5e7ae04c2c369ea56356acd2e53
Kofi Akyea <kakyea13@gmail.com>
2024-03-13 07:39:57 +0000

    fixes

--- send_email.php
+++ send_email.php
@@ -1,16 +1,16 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
+    
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // Set up email
+  
     $to = "kakyea13@gmail.com";
     $subject = "New Submission from Portfolio";
     $body = "Name: $name\nEmail: $email\nMessage: $message";
 
-    // Send email
+  
     if (mail($to, $subject, $body)) {
         http_response_code(200);
         echo json_encode(array("message" => "Thank you for your submission"));
